2e8d96830af5609c9a32d546d641c8b0abbe732f
Phillip Fernberg <pfernb1@lsu.edu>
2018-08-21 20:40:42 -0500

    :dolphin:

---
+++ hello-world.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Description: a simple python script
+"""
+# this is a semi-useful comment
+
+"""
+This is a super useful comment thingy!!
+Yay!!!!!
+"""
+
+#set variables
+world_adjective = "new"
+
+
+#print statement
+
+print "Hello {adj} world!".format(adj=world_adjective)
+
