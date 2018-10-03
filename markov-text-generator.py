b6f306a9dfdf6545f4412fe3f1b34a2bab03d26d
Phillip Fernberg <pfernb1@lsu.edu>
2018-10-02 20:39:05 -0500

    :bowtie:

--- markov-text-generator.py
+++ markov-text-generator.py
@@ -8,16 +8,27 @@ Created on Tue Sep 11 19:00:14 2018
 
 
 # import libraries
-import markovify
+import markovify as m
 
 # get raw text as string
-with open("TheWasteland_TSEliot.txt") as f:
-    text = f.read()
+with open("TheWasteland_TSEliot.txt") as wasteland:
+    wasteland = wasteland.read()
+
+with open("Poems1918-21_EzraPound.txt") as pound:
+    pound = pound.read()
+
+with open("williams_improvisations.txt") as improv:
+    improv = improv.read()
     
-    # build the model
-text_model = markovify.NewlineText(text) #text_model = markovify.Text(text)
+# build the model
+wasteland_model = m.NewlineText(wasteland) #text_model = markovify.Text(text)
+pound_model = m.NewlineText(pound)
+improv_model = m.NewlineText(improv)
+
+synthesized_model = m.combine([wasteland_model, pound_model, improv_model], [1,1,1.5])
 
 # print three randomly-generated sentences of no more than 140 characters
-for i in range(3):
-    print text_model.make_short_sentence(140)
+for i in range(6):
+    print synthesized_model.make_short_sentence(140)
+    print " "
     
