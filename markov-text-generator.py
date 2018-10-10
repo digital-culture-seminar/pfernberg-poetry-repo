b4fbf0e79364757f942fbe43ff94a0091442533e
Phillip Fernberg <pfernb1@lsu.edu>
2018-10-10 11:10:21 -0500

    :bowtie:

--- markov-text-generator.py
+++ markov-text-generator.py
@@ -28,8 +28,48 @@ improv_model = m.NewlineText(improv)
 # synthesize the model
 synthesized_model = m.combine([wasteland_model, pound_model, improv_model], [1,1,1.5])
 
-# print three randomly-generated sentences of no more than 140 characters
-for i in range(6):
-    print synthesized_model.make_sentence()
-   
+## print randomly-generated sentences 
+#for i in range(0,11):
+#    print synthesized_model.make_sentence()
     
+# print randomly-generated sentences using lists
+my_list = []
+for i in range(0,11):
+    my_list.append(synthesized_model.make_sentence(140))
+
+# get raw text as string with (w) write or (a) append option
+with open("generated-poem.md", "w") as f:
+    
+# write the text generated from the markov model
+    for item in my_list:
+        f.write(item)
+
+    
+#print to variable 
+#with open("generated-poem.md", "w") as f:
+#    f.write("## Retrospective Collaboration")
+#    f.write("\n")
+#    f.write("\n")
+#    for i in range(3):
+#        poem = synthesized_model.make_sentence() 
+#        print synthesized_model.make_sentence()
+#        f.write(poem)
+#        f.write("\n")
+    
+    
+#with open("generated-poem.md", "w") as f:
+#    f.write("## Title")
+#    f.write("\n")
+#    f.write("\n")
+#    f.write("```")
+#    f.write(poem)
+#    f.write("```")
+
+
+#with open("generated-poem.md", "w") as f:
+#    f.write("""
+## Title
+#```
+#{poem}
+#```
+#""".format(poem=poem))
