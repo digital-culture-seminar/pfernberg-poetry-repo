41c4c74dd3039f1240092f3a5d9b0f153cd20432
Phillip Fernberg <pfernb1@lsu.edu>
2018-10-16 20:06:55 -0500

    script creation

--- scripts/markovify-to-speech.py
+++ scripts/markovify-to-speech.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep 11 19:00:14 2018
+Created on Tue Oct 16 19:05:36 2018
 
 @author: Phil
 """
-
-
 # import libraries
 import markovify as m
+from playsound import playsound
+from gtts import gTTS
 
 # get raw text as string
-with open("../TheWasteland_TSEliot.txt") as wasteland:
+with open("../poems/TheWasteland_TSEliot.txt") as wasteland:
     wasteland = wasteland.read()
 
-with open("../Poems1918-21_EzraPound.txt") as pound:
+with open("../poems/Poems1918-21_EzraPound.txt") as pound:
     pound = pound.read()
 
-with open("../williams_improvisations.txt") as improv:
+with open("../poems/williams_improvisations.txt") as improv:
     improv = improv.read()
     
 # build the model
-wasteland_model = m.NewlineText(wasteland) #text_model = markovify.Text(text)
-pound_model = m.NewlineText(pound)
-improv_model = m.NewlineText(improv)
+wasteland_model = m.Text(wasteland) #text_model = markovify.Text(text)
+pound_model = m.Text(pound)
+improv_model = m.Text(improv)
 
 # synthesize the model
 synthesized_model = m.combine([wasteland_model, pound_model, improv_model], [1,1,1.5])
@@ -35,10 +35,11 @@ synthesized_model = m.combine([wasteland_model, pound_model, improv_model], [1,1
 # print randomly-generated sentences using lists
 my_list = []
 for i in range(1,11):
-    my_list.append(synthesized_model.make_short_sentence(140))
+    my_list.append(synthesized_model.make_short_sentence(140) + \
+                   synthesized_model.make_short_sentence(140))
 
  # get raw text as string with (w) write or (a) append option
-with open("markov-generated-poem-iterations.md", "a") as f:
+with open("../poems/markov-generated-poem-iterations.md", "a") as f:
     
 # write the text generated from the markov model
     for item in my_list:
@@ -73,3 +74,12 @@ with open("markov-generated-poem-iterations.md", "a") as f:
 #{poem}
 #```
 #""".format(poem=poem))
+
+# text to speech
+tts = gTTS(text=markov_poem, lang='en')
+
+# write audio file
+tts.save("../poems/markovified-poem.mp3")
+
+# play audio file
+playsound("../poems/markovified-poem.mp3")
